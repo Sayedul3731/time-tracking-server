658b36ddc6be140b225ad565b3017b25166795b6
MD Sayedul Haque <mdsayedulhaque932@gmail.com>
2024-01-09 14:04:43 +0600

    new project data get from client side and save in database

--- index.js
+++ index.js
@@ -25,13 +25,21 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const projectCollection = client.db('timeDB').collection('projects');
+
+        app.post('/projects', async(req,res)=>{
+            const newInfo = req.body;
+            const result = await projectCollection.insertOne(newInfo)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
